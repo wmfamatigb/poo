40eb07d975123f57616a83bf0ad22962dd73bed4
test <test@test.test>
2021-05-24 00:51:40 +0200

    put image arch

--- src/main/java/cli/Cli.java
+++ src/main/java/cli/Cli.java
@@ -8,12 +8,13 @@ import java.util.Scanner;
 public class Cli {
 
     public  static CliState state = CliState.MAIN_MENU;
+    private static Map<CliState, CliMenu> menuDictionary = new HashMap();
 
-    private static Map<CliState, CliMenu> handlerMapper = new HashMap();
-
-    static{
-        handlerMapper.put(CliState.MAIN_MENU, new MainMenu());
-        handlerMapper.put(CliState.STUDENT_MENU, new StudentMenu());
+    static{ // static bloc because we want to initialise a static variable
+        // init dictionary with all state -> menu translations
+        menuDictionary.put(CliState.MAIN_MENU, new MainMenu());
+        menuDictionary.put(CliState.STUDENT_MENU, new StudentMenu());
+        menuDictionary.put(CliState.EMPLOYEE_MENU, new EmployeeMenu());
     }
 
 
@@ -22,18 +23,20 @@ public class Cli {
 
         while(true){
             System.out.println("Please select an option");
-            CliMenu currentMenu = handlerMapper.get(state);
+            CliMenu currentMenu = menuDictionary.get(state);
             int i =0;
-            for(String option :  currentMenu.getOptions() ){
+
+
+            for(String option :  currentMenu.getOptions() ){ // a java way of iterating over a list of elements
                 System.out.println(++i + ". " + option);
             }
             String userSelectedOption = getUserInput();
 
             if(!currentMenu.supports(userSelectedOption)){
                 System.out.print("Unsupported operation: '" + userSelectedOption +"', allowed values: " );
-                currentMenu.getOptions().forEach(e -> System.out.print(e + " "));
+                currentMenu.getOptions().forEach(e -> System.out.print(e + " ")); // another java way to iterate over a list of elements
                 System.out.println();
-                continue;
+                continue; // return to the begging of the loop
             }
 
             currentMenu.handleOption(userSelectedOption);
